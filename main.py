40319588c492212f6ea90be51ad4fc5c1a53c795
aleksh90 <aleks.hughes@uk.ibm.com>
2016-12-09 16:01:23 +0000

    initial commit - application bare bones design

---
+++ main.py
@@ -0,0 +1,24 @@
+# var/lib/python
+
+import selenium
+
+# custom class:
+# snippet object
+
+#_______________________#_______________________#_______________________#_______________________
+# view the snippets
+# import the snippets - assign a key, import metadata
+#
+
+
+# buttons:
+# toggle between English and Spanish
+# default single word
+
+#_______________________#_______________________#_______________________#_______________________
+#NICE TO HAVE
+# lookup official meaning
+# automatic English/Spanish detection
+
+
+
